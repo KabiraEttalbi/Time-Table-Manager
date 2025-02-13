38765e2e094eff626bd713e9dce7d3ecf6bb8ec7
CoderOuma <oumaymarochdi01@gmail.com>
2025-02-13 04:53:01 +0100

    Fix: Add module data fetching and process in modulesData for page.tsx
    - Ajout de la récupération des données des modules avec l'intégration des informations du niveau et de l'option associées (basées sur les IDs).
    - Mise à jour du code de `modulesData` pour inclure les noms de niveau et d'option dans les données des modules.
    - Modification de `page.tsx` pour afficher correctement ces informations dans le tableau des modules.

--- frontend/app/(dashboard)/list/modules/page.tsx
+++ frontend/app/(dashboard)/list/modules/page.tsx
@@ -2,29 +2,21 @@ import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { lessonsData, role } from "@/lib/data";
+import { Module, modulesData, role } from "@/lib/data";
 import Image from "next/image";
-
-type Lesson = {
-  id: number;
-  subject: string;
-  class: string;
-  teacher: string;
-};
 
 const columns = [
   {
-    header: "Subject Name",
+    header: "Nom du Module",
     accessor: "name",
   },
   {
-    header: "Class",
-    accessor: "class",
+    header: "Description",
+    accessor: "description",
   },
   {
-    header: "Teacher",
-    accessor: "teacher",
-    className: "hidden md:table-cell",
+    header: "Option",
+    accessor: "option.name",
   },
   {
     header: "Actions",
@@ -32,21 +24,30 @@ const columns = [
   },
 ];
 
-const LessonListPage = () => {
-  const renderRow = (item: Lesson) => (
+const ModuleListPage = async () => {
+  const { modules } = await modulesData();
+
+  // Vérifier si 'modules' est un tableau
+  if (!Array.isArray(modules)) {
+    return <div>Error: Data is not an array</div>;
+  }
+
+  console.log("Modules Data in Component:", modules); // Afficher les modules ici
+
+  const renderRow = (item: Module) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
     >
-      <td className="flex items-center gap-4 p-4">{item.subject}</td>
-      <td>{item.class}</td>
-      <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="flex items-center gap-4 p-4">{item.name}</td>
+      <td className="hidden md:table-cell">{item.description}</td>
+      <td className="hidden md:table-cell">{item.option?.name}</td>
       <td>
         <div className="flex items-center gap-2">
           {role === "admin" && (
             <>
-              <FormModal table="lesson" type="update" data={item} />
-              <FormModal table="lesson" type="delete" id={item.id} />
+              <FormModal table="modules" type="update" data={item} />
+              <FormModal table="modules" type="delete" id={item.id} />
             </>
           )}
         </div>
@@ -56,9 +57,8 @@ const LessonListPage = () => {
 
   return (
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
-      {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Lessons</h1>
+        <h1 className="hidden md:block text-lg font-semibold">Modules</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -68,16 +68,17 @@ const LessonListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
-            {role === "admin" && <FormModal table="lesson" type="create" />}
+            {role === "admin" && <FormModal table="modules" type="create" />}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={lessonsData} />
+      <Table columns={columns} renderRow={renderRow} data={modules} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default LessonListPage;
+
+export default ModuleListPage;
