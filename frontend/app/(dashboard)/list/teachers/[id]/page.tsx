2c3071e24c04f58cdeeec818186fe3bcf8480ce0
CoderOuma <oumaymarochdi01@gmail.com>
2025-02-21 12:13:16 +0100

    fix: Update teacher page to handle async params and improve module selection

--- frontend/app/(dashboard)/list/teachers/[id]/page.tsx
+++ frontend/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -13,14 +13,16 @@ import React from "react";
 export const { schedules } = await schedulesData();
 console.log(schedules)
 
-const SingleTeacherPage = ({ params }) => {
+const SingleTeacherPage = async ({ params }: { params: { id: string } }) => {
+  
   const user = useUser(); // Retrieve the user object from context
   const role = user?.role || ''; // Extract the role from the user object
 
-  // Unwrap params using React.use()
-  const { id } = React.use(params); // Get teacher ID from URL
-
+  const { id } = await params; // Get teacher ID from URL
   const teacher = teachers.find((teacher) => teacher._id === id);
+
+  
+  
   // Check if teacher exists
   if (!teacher) {
     return <div>Teacher not found</div>;
